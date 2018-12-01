1991bdea1461a4310a4a67e6eca9ce3c790322b7
yangxuqiang9 <932477876@qq.com>
2018-12-01 16:56:52 +0800

    commit

--- fanweHybridLive/src/main/java/com/fanwe/live/activity/room/LiveVideoScrollActivity.java
+++ fanweHybridLive/src/main/java/com/fanwe/live/activity/room/LiveVideoScrollActivity.java
@@ -29,49 +29,49 @@ public class LiveVideoScrollActivity extends LiveLayoutViewerExtendActivity {
     protected void init(Bundle savedInstanceState) {
         super.init(savedInstanceState);
 
-//        verticalViewPager = (VerticalViewPager) findViewById(R.id.verticalviewpager);
+        verticalViewPager = (VerticalViewPager) findViewById(R.id.verticalviewpager);
 
 
-//        verticalViewPager.setAdapter(new DummyAdapter(getSupportFragmentManager()));
-//        verticalViewPager.setPageMargin(getResources().getDimensionPixelSize(R.dimen.pagemargin));
-//        verticalViewPager.setPageMarginDrawable(new ColorDrawable(getResources().getColor(android.R.color.holo_green_dark)));
-//
-//        verticalViewPager.setPageTransformer(true, new ViewPager.PageTransformer() {
-//            @Override
-//            public void transformPage(View view, float position) {
-//                int pageWidth = view.getWidth();
-//                int pageHeight = view.getHeight();
-//
-//                if (position < -1) { // [-Infinity,-1)
-//                    // This page is way off-screen to the left.
-//                    view.setAlpha(0);
-//
-//                } else if (position <= 1) { // [-1,1]
-//                    // Modify the default slide transition to shrink the page as well
-//                    float scaleFactor = Math.max(MIN_SCALE, 1 - Math.abs(position));
-//                    float vertMargin = pageHeight * (1 - scaleFactor) / 2;
-//                    float horzMargin = pageWidth * (1 - scaleFactor) / 2;
-//                    if (position < 0) {
-//                        view.setTranslationY(vertMargin - horzMargin / 2);
-//                    } else {
-//                        view.setTranslationY(-vertMargin + horzMargin / 2);
-//                    }
-//
-//                    // Scale the page down (between MIN_SCALE and 1)
-//                    view.setScaleX(scaleFactor);
-//                    view.setScaleY(scaleFactor);
-//
-//                    // Fade the page relative to its size.
-//                    view.setAlpha(MIN_ALPHA +
-//                            (scaleFactor - MIN_SCALE) /
-//                                    (1 - MIN_SCALE) * (1 - MIN_ALPHA));
-//
-//                } else { // (1,+Infinity]
-//                    // This page is way off-screen to the right.
-//                    view.setAlpha(0);
-//                }
-//            }
-//        });
+        verticalViewPager.setAdapter(new DummyAdapter(getSupportFragmentManager()));
+        verticalViewPager.setPageMargin(getResources().getDimensionPixelSize(R.dimen.pagemargin));
+        verticalViewPager.setPageMarginDrawable(new ColorDrawable(getResources().getColor(android.R.color.holo_green_dark)));
+
+        verticalViewPager.setPageTransformer(true, new ViewPager.PageTransformer() {
+            @Override
+            public void transformPage(View view, float position) {
+                int pageWidth = view.getWidth();
+                int pageHeight = view.getHeight();
+
+                if (position < -1) { // [-Infinity,-1)
+                    // This page is way off-screen to the left.
+                    view.setAlpha(0);
+
+                } else if (position <= 1) { // [-1,1]
+                    // Modify the default slide transition to shrink the page as well
+                    float scaleFactor = Math.max(MIN_SCALE, 1 - Math.abs(position));
+                    float vertMargin = pageHeight * (1 - scaleFactor) / 2;
+                    float horzMargin = pageWidth * (1 - scaleFactor) / 2;
+                    if (position < 0) {
+                        view.setTranslationY(vertMargin - horzMargin / 2);
+                    } else {
+                        view.setTranslationY(-vertMargin + horzMargin / 2);
+                    }
+
+                    // Scale the page down (between MIN_SCALE and 1)
+                    view.setScaleX(scaleFactor);
+                    view.setScaleY(scaleFactor);
+
+                    // Fade the page relative to its size.
+                    view.setAlpha(MIN_ALPHA +
+                            (scaleFactor - MIN_SCALE) /
+                                    (1 - MIN_SCALE) * (1 - MIN_ALPHA));
+
+                } else { // (1,+Infinity]
+                    // This page is way off-screen to the right.
+                    view.setAlpha(0);
+                }
+            }
+        });
     }
 
     public class DummyAdapter extends FragmentPagerAdapter {
